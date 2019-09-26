b73283b624aec1524e5575f343ad6bf4f2d9ba31
Harshit kumar <harshitrudrapur@gmail.com>
2019-09-26 10:23:46 +0000

    adding current files

---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var app= express();
+var request = require('request');
+var bodyparser= require('body-parser');
+
+//linking the css files to main 
+app.use(express.static("decfiles"));
+
+//making our routes
+app.get("/",(req,res)=>{
+	res.render("page.ejs");
+});
+
+app.get("/aboutpage",(req,res)=>{
+	res.render("aboutme.ejs");
+});
+
+app.get("/infopage",(req,res)=>{
+    var a= req.query.t;
+	var b= req.query.type;
+	var c= req.query.y;
+	var d= req.query.plot;	
+	
+	var url= "http://omdbapi.com/?t="+a+"&type="+b+"&y="+c+"&plot="+d+"&apikey=thewdb";
+	//i will place my api request here
+	request(url,(error,response,body)=>{
+		if(!error && response.statusCode==200){
+			var data=  JSON.parse(body);
+			 
+			res.render("infopage.ejs",{data:data});	
+			//res.send(data["Search"]);
+		}
+		else{
+			res.send("Check FoR ErrOR");
+     }
+	});
+});
+
+app.get("/movie",(req,res)=>{
+	var x= req.query.k;
+	var y= req.query.t;
+	var z= req.query.y;
+	
+	var url= "http://omdbapi.com/?s="+x+"&type="+y+"&y="+z+"&apikey=thewdb";
+	//i will place my api request here
+	request(url,(error,response,body)=>{
+		if(!error && response.statusCode==200){
+			var data=  JSON.parse(body);
+			 
+			res.render("res.ejs",{data:data});	
+			//res.send(data["Search"]);
+		}
+		else{
+			res.send("Check FoR ErrOR");
+     }
+	});
+});
+
+
+
+app.listen(1111,()=>{
+	console.log("server started");
+});
+
